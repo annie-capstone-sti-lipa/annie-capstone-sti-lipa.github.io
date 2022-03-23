6902af718f57508b20a29e8745bb6484c95e0112
Jerome <josejeromelalunio@gmail.com>
2022-03-24 05:08:51 +0800

    setup basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.scss";
+import Body from "./components/body/body";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div id="app">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
