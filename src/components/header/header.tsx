036b17fee389c0f0c06ed67921f6cb518f68e836
Jerome <josejeromelalunio@gmail.com>
2022-03-30 11:35:40 +0800

    add header components

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,16 @@
 import "./header.scss";
 
 export default function Header() {
-  return <div id="header">Header</div>;
+  return (
+    <div id="header">
+      <div className="container">
+        <div className="header-container">
+          <div>user image</div>
+          <div>name and bio</div>
+          <div>kanji quiz score</div>
+          <div>mal discord</div>
+        </div>
+      </div>
+    </div>
+  );
 }
