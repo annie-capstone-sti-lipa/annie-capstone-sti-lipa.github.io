1499435843ff3883001bfd4843ddcfc8d558e31c
Jerome <josejeromelalunio@gmail.com>
2022-04-06 15:34:55 +0800

    make tabnav responsive

--- src/components/header/tabnav/tabnav.tsx
+++ src/components/header/tabnav/tabnav.tsx
@@ -3,17 +3,24 @@ import { select } from "../../../redux/reducers/tabnav-item-reducer";
 import tabnavItem from "../../../types/enums/tabnavItem";
 import "./tabnav.scss";
 
+import rightIcon from "../../../assets/icons/right.svg";
+import leftIcon from "../../../assets/icons/left.svg";
+
 export default function Tabnav() {
   return (
-    <div className="container">
-      <div className="tabnav">
-        {Object.keys(tabnavItem).map((key, index) => (
-          <TabnavItem
-            name={Object.values(tabnavItem)[index]}
-            key={key + index}
-          />
-        ))}
+    <div className="arrow-container">
+      <img className="left arrow" src={leftIcon} alt="left icon" />
+      <div className="container">
+        <div className="tabnav">
+          {Object.keys(tabnavItem).map((key, index) => (
+            <TabnavItem
+              name={Object.values(tabnavItem)[index]}
+              key={key + index}
+            />
+          ))}
+        </div>
       </div>
+      <img className="right arrow" src={rightIcon} alt="right icon" />
     </div>
   );
 }
