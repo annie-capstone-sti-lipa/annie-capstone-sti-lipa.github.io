104a2ace93215a5f0e6e39451640d1f7ac9c2235
Jerome <josejeromelalunio@gmail.com>
2022-04-06 17:29:04 +0800

    add ui for uploading image

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -1,8 +1,23 @@
 import { useSelector } from "react-redux";
+import tabnavItem from "../../types/enums/tabnavItem";
 import "./body.scss";
+import SauceFinder from "./sauce-finder/sauce-finder";
 
 export default function Body() {
   const selected = useSelector((state: any) => state.tabnav.selected);
 
-  return <div id="body">{selected}</div>;
+  function bodySwitch() {
+    switch (selected) {
+      case tabnavItem.sauceFinder:
+        return <SauceFinder />;
+      default:
+        return <div>{selected}</div>;
+    }
+  }
+
+  return (
+    <div className="container">
+      <div id="body">{bodySwitch()}</div>
+    </div>
+  );
 }
---
+++ src/components/body/sauce-finder/sauce-finder.tsx
@@ -0,0 +1,48 @@
+import "./sauce-finder.scss";
+
+import addImageIcon from "../../../assets/icons/add_image.svg";
+import { useState } from "react";
+import EditIcon from "../../edit-icon/edit-icon";
+
+export default function SauceFinder() {
+  const [image, setImage] = useState<any>(addImageIcon);
+
+  return (
+    <div className="sauce-finder">
+      <div className="hint-text">Select Image:</div>
+      <div className="image-input">
+        <div className="preview-image-container">
+          <EditIcon />
+          <img
+            className="preview-image"
+            src={image}
+            alt="upload"
+            onClick={() => {
+              document.getElementById("image-input")?.click();
+            }}
+          />
+        </div>
+        <input
+          id="image-input"
+          type="file"
+          accept="image/*"
+          onChange={(event) => {
+            if (event.target.files && event.target.files[0]) {
+              let reader = new FileReader();
+              reader.onload = (e) => {
+                setImage(() => e.target?.result);
+              };
+              reader.readAsDataURL(event.target.files[0]);
+            }
+          }}
+        />
+      </div>
+      <div className="or">or</div>
+      <div>
+        <span className="hint-text">Paste image URL:</span>
+        <input type="text" />
+      </div>
+      <div className="search-button">Search</div>
+    </div>
+  );
+}
