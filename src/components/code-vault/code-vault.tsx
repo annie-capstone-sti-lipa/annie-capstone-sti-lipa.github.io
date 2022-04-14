1891b2d5cd299248dd17f0e3672adef3178c1992
Jerome <josejeromelalunio@gmail.com>
2022-04-15 07:09:42 +0800

    add numbers input

--- src/components/code-vault/code-vault.tsx
+++ src/components/code-vault/code-vault.tsx
@@ -1,14 +1,47 @@
 import "./code-vault.scss";
 
 import addIcon from "../../assets/icons/add.svg";
+import { useState } from "react";
+import Modal from "../general/modal/modal";
+
+import theNumbers from "../../assets/images/numbers.jpg";
 
 export default function CodeVault() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="code-vault">
+      <Modal
+        showModal={showModal}
+        closeModal={() => setShowModal(() => false)}
+        body={<ModalBody closeModal={() => setShowModal(() => false)} />}
+      />
       <div className="button-container">
-        <div className="add-button">
+        <div className="add-button" onClick={() => setShowModal(true)}>
           <img className="add-icon" src={addIcon} alt="add code" />
           <span>Add</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ModalBody({ closeModal }: { closeModal: () => void }) {
+  return (
+    <div className="code-modal" onClick={(event) => event.stopPropagation()}>
+      <img
+        className="numbers-image"
+        src={theNumbers}
+        alt="What are the numbers ?"
+      />
+      <div className="question-container">
+        <div className="input-container">
+          <div className="question">What are the numbers?</div>
+          <input className="input" type="text" placeholder="777777" />
+        </div>
+        <div className="submit-button">
+          <img className="submit-icon" src={addIcon} alt="add code" />
+          <span>Submit</span>
         </div>
       </div>
     </div>
