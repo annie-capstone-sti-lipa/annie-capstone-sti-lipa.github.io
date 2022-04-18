f2d59c1bdf27f6ba9d67ad362aa6882bfc28c83a
Jerome <josejeromelalunio@gmail.com>
2022-04-18 11:13:52 +0800

    add quiz ui

--- src/components/body/quiz/quiz.tsx
+++ src/components/body/quiz/quiz.tsx
@@ -1,5 +1,24 @@
 import "./quiz.scss";
 
 export default function Quiz() {
-  return <div>quizzzz</div>;
+  return (
+    <div className="quiz">
+      <div>Choose the corresponding reading for the character below.</div>
+      <CharacterCard character="せ" />
+      <div className="choice-card-container">
+        <ChoiceCard choice="sa" />
+        <ChoiceCard choice="ra" />
+        <ChoiceCard choice="pa" />
+        <ChoiceCard choice="la" />
+      </div>
+    </div>
+  );
+}
+
+function CharacterCard({ character }: { character: string }) {
+  return <div className="character-card">{character}</div>;
+}
+
+function ChoiceCard({ choice }: { choice: string }) {
+  return <div className="choice-card">{choice}</div>;
 }
