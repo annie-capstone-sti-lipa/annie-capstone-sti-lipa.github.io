d04dd07b1594af6673121b336224e321959b6361
Jerome <josejeromelalunio@gmail.com>
2022-04-18 11:37:55 +0800

    add quiz choice

--- src/components/body/quiz/quiz.tsx
+++ src/components/body/quiz/quiz.tsx
@@ -1,15 +1,65 @@
+import { useState } from "react";
 import "./quiz.scss";
 
+enum quizType {
+  hiragana = "Hiragana",
+  katakana = "Katakana",
+  kanji = "Kanji",
+}
+
 export default function Quiz() {
+  const [quizType, setQuizType] = useState<quizType | null>(null);
+
   return (
     <div className="quiz">
+      {quizType != null ? (
+        <QuizQuestions />
+      ) : (
+        <QuizChoice chooseQuiz={(choice) => setQuizType(choice)} />
+      )}
+    </div>
+  );
+}
+
+function QuizChoice({
+  chooseQuiz,
+}: {
+  chooseQuiz: (quizType: quizType) => void;
+}) {
+  return (
+    <div className="quiz-choice">
+      <div className="instruction">Choose Writing System.</div>
+      <QuizTypeCard type={quizType.hiragana} chooseQuiz={chooseQuiz} />
+      <QuizTypeCard type={quizType.katakana} chooseQuiz={chooseQuiz} />
+      <QuizTypeCard type={quizType.kanji} chooseQuiz={chooseQuiz} />
+    </div>
+  );
+}
+
+function QuizTypeCard({
+  type,
+  chooseQuiz,
+}: {
+  type: quizType;
+  chooseQuiz: (quizType: quizType) => void;
+}) {
+  return (
+    <div className="quiz-type-card" onClick={() => chooseQuiz(type)}>
+      {type}
+    </div>
+  );
+}
+
+function QuizQuestions() {
+  return (
+    <div className="quiz-questions">
       <div>Choose the corresponding reading for the character below.</div>
       <CharacterCard character="せ" />
       <div className="choice-card-container">
-        <ChoiceCard choice="sa" />
-        <ChoiceCard choice="ra" />
-        <ChoiceCard choice="pa" />
-        <ChoiceCard choice="la" />
+        <ChoiceCard choice="se" />
+        <ChoiceCard choice="re" />
+        <ChoiceCard choice="pe" />
+        <ChoiceCard choice="le" />
       </div>
     </div>
   );
