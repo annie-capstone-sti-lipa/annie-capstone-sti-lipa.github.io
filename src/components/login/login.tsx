44769528502eeedc7209b78cc7a6ac248665c70f
Grrom <josejeromelalunio@gmail.com>
2022-09-08 12:03:20 +0800

    add login and signup form

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -4,11 +4,17 @@ import "./login.scss";
 function LoginFrame() {
   const [isLogin, setIsLogin] = useState(true);
 
+  const [formKey, setFormKey] = useState(1);
+
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
   const [signupUsername, setSignupUsername] = useState("");
   const [signupPassword, setSignupPassword] = useState("");
   const [signupPassword1, setSignupPassword1] = useState("");
 
   const [signupError, setSignupError] = useState("");
+  const [loginError, setLoginError] = useState("");
 
   useEffect(() => {
     if (document.querySelector(".error-text") !== null) {
@@ -16,17 +22,52 @@ function LoginFrame() {
     }
   }, [signupError]);
 
+  useEffect(() => {
+    if (document.querySelector(".error-text") !== null) {
+      document.querySelector(".error-text")!.innerHTML = loginError;
+    }
+  }, [loginError]);
+
+  useEffect(() => {
+    setFormKey(Math.random());
+  }, [isLogin]);
+
   if (isLogin) {
     return (
-      <div id="login" key={Math.random()}>
+      <div id="login" key={formKey}>
         <div className="login-form-container">
           <div className="login-form">
             <div className="login-title">Login</div>
             <small>username</small>
-            <input type="text" className="input-field" />
+            <input
+              type="text"
+              defaultValue={loginUsername}
+              className="input-field"
+              onChange={(event) => setLoginUsername(event.target.value)}
+            />
             <small>password</small>
-            <input type="password" className="input-field" />
-            <div className="submit-button">Login</div>
+            <input
+              type="password"
+              defaultValue={loginPassword}
+              className="input-field"
+              onChange={(event) => setLoginPassword(event.target.value)}
+            />
+            <small className="error-text"></small>
+            <div
+              className="submit-button"
+              onClick={() => {
+                setLoginError("");
+                if (true) {
+                  setLoginError("sample login error");
+                }
+
+                if (loginError === "") {
+                  alert(loginUsername + " " + loginPassword);
+                }
+              }}
+            >
+              Login
+            </div>
             <div>Don't have an account yet? </div>
             <div className="signup-cta" onClick={() => setIsLogin(false)}>
               Sign up.
@@ -45,18 +86,21 @@ function LoginFrame() {
           <small>username</small>
           <input
             type="text"
+            defaultValue={signupUsername}
             className="input-field"
             onChange={(event) => setSignupUsername(event.target.value)}
           />
           <small>password</small>
           <input
             type="password"
+            defaultValue={signupPassword}
             className="input-field"
             onChange={(event) => setSignupPassword(event.target.value)}
           />
           <small>confirm password</small>
           <input
             type="password"
+            defaultValue={signupPassword1}
             className="input-field"
             onChange={(event) => setSignupPassword1(event.target.value)}
           />
@@ -67,6 +111,10 @@ function LoginFrame() {
               setSignupError("");
               if (signupPassword !== signupPassword1) {
                 setSignupError("Passwords do not match.");
+              }
+
+              if (signupError === "") {
+                alert(signupUsername + " " + signupPassword);
               }
             }}
           >
