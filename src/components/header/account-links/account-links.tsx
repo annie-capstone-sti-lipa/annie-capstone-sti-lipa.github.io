09f058d8b230bd3b71c5fb1b383fac1bdc922010
Grrom <josejeromelalunio@gmail.com>
2022-11-02 12:54:17 +0800

    add discord modal

--- src/components/header/account-links/account-links.tsx
+++ src/components/header/account-links/account-links.tsx
@@ -5,22 +5,41 @@ import malIcon from "../../../assets/icons/mal.svg";
 import AnnieAPI from "../../../helpers/annie-api";
 import AlertHelper from "../../../helpers/alert-helper";
 import { useSelector } from "react-redux";
+import Modal from "../../general/modal/modal";
+import { useState } from "react";
+import DiscordAuth from "../../../types/discord-auth";
 
 export default function AccountLinks() {
+  const [showDiscordModal, setShowDiscordModal] = useState(false);
+  const [discordAuth, setDiscordAuth] = useState<DiscordAuth>();
   const user = useSelector((state: any) => state.isLoggedIn.user);
 
   return (
     <div className="account-links">
+      <Modal
+        showModal={showDiscordModal}
+        closeModal={() => setShowDiscordModal(() => false)}
+        body={
+          <DiscordModalBody
+            discordAuth={discordAuth}
+            closeModal={() => setShowDiscordModal(() => false)}
+          />
+        }
+      />
       <AccountButton
         icon={discordIcon}
         name="Discord"
         color="#5865f2"
         onClick={() => {
           let discordLoading = AlertHelper.showLoading("Please Wait...");
-          AnnieAPI.getDiscordAuth(user.uid).then((discordAuth) => {
-            discordLoading.close();
-            console.log(discordAuth);
-          });
+          AnnieAPI.getDiscordAuth(user.uid)
+            .then((discordAuth) => {
+              setDiscordAuth(discordAuth);
+              setShowDiscordModal(true);
+            })
+            .finally(() => {
+              discordLoading.close();
+            });
         }}
       />
       <AccountButton
@@ -40,6 +59,37 @@ export default function AccountLinks() {
   );
 }
 
+function DiscordModalBody({
+  closeModal,
+  discordAuth,
+}: {
+  closeModal: () => void;
+  discordAuth?: DiscordAuth;
+}) {
+  return (
+    <div className="discord-modal" onClick={(event) => event.stopPropagation()}>
+      <div className="discord-message">
+        <div>Register by sending this command to Annie.</div>
+        <div className="register-command">
+          <span className="command">{discordAuth?.message}</span>
+          <small
+            className="copy-command"
+            onClick={() => {
+              navigator.clipboard.writeText(discordAuth!.message);
+              AlertHelper.successToast("Command Copied to clipboard.");
+            }}
+          >
+            copy command
+          </small>
+        </div>
+        <div />
+        <a href={discordAuth?.link} className="discord-link" target="_blank">
+          Message Annie
+        </a>
+      </div>
+    </div>
+  );
+}
 function AccountButton({
   icon,
   name,
